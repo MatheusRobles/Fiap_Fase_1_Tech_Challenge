b1e4642316d10034ea44d76ea18382cc46223499
Matheus Robles <92039339+MatheusRobles@users.noreply.github.com>
2024-01-26 10:56:21 -0300

    Update tabela.py

--- tabela.py
+++ tabela.py
@@ -1,13 +1,1 @@
-import streamlit as st
-from functions import *
 
-
-
-
-def tabela(df):
-    st.markdown(
-        """
-        ###### DataFrame com todos as informações utilizadas na análise.
-        """
-    )
-    st.dataframe(df)
